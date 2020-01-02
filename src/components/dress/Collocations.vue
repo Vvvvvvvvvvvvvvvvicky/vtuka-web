f8e0019b700b030b5f8506d7a875e59883ab3716
Vvvvvvvvvvvvvvvvicky <vicky.h.m.lee@outlook.com>
2020-01-02 23:35:46 +0800

    update

--- src/components/dress/Collocations.vue
+++ src/components/dress/Collocations.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <el-row style="height: 1000px;">
+    <el-row >
       <!--<search-bar></search-bar>-->
       <el-tooltip effect="dark" placement="right"
-                  v-for="item in books"
+                  v-for="item in callocations"
                   :key="item.id">
        
         <p slot="content" style="width: 300px" class="abstract">{{item.collocationType}}</p>
@@ -20,13 +20,18 @@
         </el-card>
       </el-tooltip>
     </el-row>
-    <el-row>
-      <el-pagination
-        @current-change="handleCurrentChange"
-        :current-page="currentPage"
-        :page-size="pagesize"
-        :total="books.length">
-      </el-pagination>
+    <el-row class="pagination">
+      <div align="middle" padding="100px">
+       <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="pageSizes"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+      </div>
     </el-row>
   </div>
 </template>
@@ -36,29 +41,45 @@
     name: 'Collocations',
     data () {
       return {
-        books: []
+        callocations: [],
+        currentPage: 1,
+        pageSizes: [10, 20, 30, 40, 50],
+        pagesize: 10,
+        total:0
       }
     },
     mounted: function () {
-      this.loadBooks()
+      this.loadCallocations()
     },
     methods:{
-      loadBooks () {
+      loadCallocations () {
         var _this = this
         this.$axios.get('/collocations').then(resp => {
           if (resp && resp.status === 200) {
-            _this.books = resp.data.resultObj.list
+            _this.callocations = resp.data.resultObj.list
+            _this.total = resp.data.resultObj.total
           }
         })
       },
       handleCurrentChange: function (currentPage) {
         this.currentPage = currentPage
+      },
+      handleSizeChange: function(pagesize) {
+        this.pagesize = pagesize
       }
     }
   }
 </script>
 
 <style scoped>
+  .pagination {
+    height: 240px;
+  }
+
+  .pagination_div {
+    
+  }
+
   .cover {
     /*width: 455px;
     height: 172px;*/
@@ -97,6 +118,11 @@
 
   a:link, a:visited, a:focus {
     color: #3377aa;
+  }
+
+  .el-pagination {
+    padding: 100px;
+    font-weight: 500;
   }
 </style>
 
